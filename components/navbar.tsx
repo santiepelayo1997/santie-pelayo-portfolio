eab67ed7f1b73175a91c50ae15b2545854a3a422
Santie Pelayo <santiepelayo1997@gmail.com>
2025-05-05 22:02:25 +0800

    Fixed second error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react"
 import Link from "next/link"
 import { Menu, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { motion } from "framer-motion"
 
